a8b7143575ea0f4af6d45dd6c13038cd6cec4b53
Lorin Klugman <lorin@public-action.org>
2013-12-21 13:44:44 -0600

    Added validations for the command line. Also added some better status messages (E.G. Validating Command Line, Processing File, Persisting Data, etc.)

--- src/skill_data_mine/core.clj
+++ src/skill_data_mine/core.clj
@@ -5,26 +5,49 @@
   (:use skill-data-mine.datetimeUtil)
 )
 (use '[datomic.api :only [q db] :as d])
+(use 'clojure.java.io)
 
+(defn file-exists? [filepath]
+  (if (.exists (as-file filepath)) true 
+  (throw (Exception. (format "File Does Not Exist: %s "  filepath)))))
+
+
+
+(defn valid-command-line? [conn args]
+    (println "Checking command line.")
+    (let [ num-args (count args) ]
+      (if (every? true? (seq [ (= num-args 6) (= (nth args 0) "-in") (= (nth args 2) "-name") (= (nth args 4) "-d")] ))
+        (do (try 
+           (file-exists? (nth args 1))
+           (snapshot-description-not-exists? conn (nth args 3))
+           (valid-cl-date? (nth args 5))
+           (catch Exception e 
+             (do (println (format "Exception: %s" (.getMessage e)))
+               (System/exit 0)))))
+        (do
+          (println "Error: Invalid command line.")
+          (println "Usage: -in INPUTFILE -name NAME-OF-SNAPSHOT -d yyyy-MM-dd ")
+          (println "Example: -in \"data/12-20-13-rss.xml\" -name \"Dec-20-13\" -d \"2013-07-04\"  " )
+          (System/exit 0) )  
+      )))
 
 (defn -main
   "Entry Point to Program"
   [& args]
-  (dist-node-elems (get-xml-from-file "data/12-18-13-rss.xml" ))
-  ;(dist-node-elems (get-xml-from-url "http://careers.stackoverflow.com/jobs/feed?searchTerm=java+clojure&location=60610&range=10&distanceUnits=Miles" ))
-  (let [ 
-            conn (d/connect "datomic:free://localhost:4334/job-posts")
-            ;  conn (init-db "datomic:free://localhost:4334/job-posts")
-              num-nodes (count @nodeList)
-              cat-list (get-distinct-category-list)
-              description "12-18-13-rss.xml"
-            ]
-       (println (format "got %d nodess" num-nodes ))
-       (println (format "got %d skills" (count cat-list)))
-        (persist-rss-data conn @nodeList cat-list (make-time-inst 2013 12 18) description)
-        (let [num-jobs (get-job-total conn description)]
-         (display-top-stats-chart conn description 10 num-jobs)
-        )
-      )
-    
-)
+  (let [   conn (d/connect "datomic:free://localhost:4334/job-posts")
+           valid   (valid-command-line? conn args)
+           file-to-process (nth args 1)
+           snapshot-description (nth args 3)
+           date-time (make-time-inst (nth args 5))]
+            (println "Processing File")
+            (dist-node-elems (get-xml-from-file file-to-process ))
+            (let [ num-nodes (count @nodeList)
+                  cat-list (get-distinct-category-list)]
+                (println (format "got %d jobs" num-nodes ))
+                (println (format "got %d skills" (count cat-list)))
+                (println "Persisting Data")
+                (persist-rss-data conn @nodeList cat-list date-time  snapshot-description)
+                (display-top-stats-chart conn snapshot-description 10 (get-job-total conn snapshot-description))
+            )))
+
+
