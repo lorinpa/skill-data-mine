323ca17ecd3262f5d35ce6ed6308689085411fae
Lorin Klugman <lorin@public-action.org>
2013-12-21 13:42:29 -0600

    Modified make-time-inst so that it can also accept a yyy-MM-dd formatted string. Added valid-cl-date? function. Used to validate command line argument.

--- src/skill_data_mine/datetimeUtil.clj
+++ src/skill_data_mine/datetimeUtil.clj
@@ -1,12 +1,28 @@
 (ns skill-data-mine.datetimeUtil
-  (:use [clj-time.core :only (date-time)])  
+  (:use [clj-time.core :only (date-time)])
+  (:use clj-time.format)
 )
 
 
 ;; creates a date-time value acceptable
 ;; to datomic's txInstant type
-(defn make-time-inst [y m d] 
+(defn make-time-inst 
+  ([y m d] 
   (let [time-str (format "%s \"%s\"" "#inst" (date-time y m d)) 
         time-inst (read-string time-str)] 
     time-inst))
+  ([date-str]
+    (let  [ fmt (formatter "yyyy-MM-dd")
+               time-str (format "%s \"%s\"" "#inst" (parse fmt date-str)) 
+               time-inst (read-string time-str)]
+      time-inst)))
 
+
+;; used to validate command line 
+;; date time argument
+(defn valid-cl-date? [date-str]
+  (let [fmt (formatter "yyyy-MM-dd" )] 
+  (try (parse fmt date-str)
+      (catch Exception e
+        (throw (Exception. (format "%s : Is not a valid date. Usage \"-d yyyy-MM-dd\". Examples: -d \"2013-07-04\" or -d \"2013-7-4\"" date-str)))
+      ))))
