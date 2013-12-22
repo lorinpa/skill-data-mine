680f79a3cd35ae27e0194038c1a1fa81752b48b2
Lorin Klugman <lorin@public-action.org>
2013-12-22 13:29:07 -0600

    Added new tests for addtional command line requests.

--- test/skill_data_mine/core_test.clj
+++ test/skill_data_mine/core_test.clj
@@ -18,14 +18,31 @@
   (is (file-exists? "data/test-sample2-rss.xml"))
 ))
 
-(deftest test-snapshot-exists
+(deftest test-snapshot-not-exists
   (testing "tests exception is thrown for existing snapshot, true for new"
   (is (thrown? Exception (snapshot-description-not-exists? conn snapshot-description))  )
   (is (snapshot-description-not-exists? conn "nosuchthing"))
 ))
+
+;; used by command line report request
+(deftest test-snapshot-exists
+  (testing "tests exception is thrown for non-existent snapshot, true for existing"
+  (is (thrown? Exception (snapshot-description-exists? conn "nosuchthing"))  )
+  (is (snapshot-description-exists? conn snapshot-description))
+))
+
 
 (deftest test-validate-date
   (testing "tests exception is thrown for invalid date, true for valid date"
   (is (thrown? Exception (valid-cl-date? "2013-12-99"))  )
   (is (valid-cl-date?  "2013-12-21"))
 ))
+
+
+(deftest test-report-command?
+  (testing "tests we can determine whether the command line starts with a report command"
+    (let [args (vec ["-report"])]
+     (is (report-command? args))
+    )
+  )  
+)
