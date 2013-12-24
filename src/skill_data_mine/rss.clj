32bf44663cf714167be0283d05eaa4c50c1e5d1e
Lorin Klugman <lorin@public-action.org>
2013-12-24 15:19:14 -0600

    Fixed up the comments.

--- src/skill_data_mine/rss.clj
+++ src/skill_data_mine/rss.clj
@@ -30,12 +30,15 @@
 (defn create-node [title pubDate link job-key categories ]
   (Node. title pubDate link job-key categories))
 
-;; extract the :content portion of the data structure
-;; extract the key and values, then put the keys and values 
-;; back together so that we have a collection of maps
-;; extract the values we need from the map(s) (E.G. title, job-key)
-;; we now have 2 pieces of data : a  node (job), categories (vector of  skills)
-;; place the 2 pieces of data in a map and return the map as our result.
+;; Extract the :content portion of the data structure
+;; Extract the keys and values. 
+;; Put the keys and values back together in to a map.
+;; Extract the values we need from the map (E.G. title, job-key).
+;; -- Extract the node (job)
+;; -- Extract the categories (skills)
+;; We now have 2 pieces of data : a  node (job), categories (vector of  skills)
+;; Place the 2 pieces of data in a containing  map. This way we have one map to 
+;; to return.
 (defn parse-elem [elem]
    (let [ content (vec (:content elem))
      data-vals  (map (fn [z] (:content z)) content)
@@ -49,9 +52,9 @@
      categories (extract-categories data-vec)]
      (assoc {} :node (create-node title pubDate link job-key categories ) :categories categories)))
 
-;; filter the xml for items only
-;; appy our parse-elem function to each item (elem)
-;; package the results as a vector
+;; Filter the xml for items only
+;; Apply our parse-elem function to each item (elem)
+;; Package the results as a vector
 (defn dist-node-elems [data]
   (vec (map parse-elem (filter  #(if (= :item (:tag %1)) %1) data))))
 
