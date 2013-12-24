f92b83b3a54e9c7fdae9013ee114bedab0369245
Lorin Klugman <lorin@public-action.org>
2013-12-24 15:02:44 -0600

    Refactored the dist-node-elems[] function. Again, more functional. No need to explictly loop through the list. We filter the list and then just use "map" to appy our "parse-elem[]" function to each element.

--- src/skill_data_mine/rss.clj
+++ src/skill_data_mine/rss.clj
@@ -30,7 +30,12 @@
 (defn create-node [title pubDate link job-key categories ]
   (Node. title pubDate link job-key categories))
 
-
+;; extract the :content portion of the data structure
+;; extract the key and values, then put the keys and values 
+;; back together so that we have a collection of maps
+;; extract the values we need from the map(s) (E.G. title, job-key)
+;; we now have 2 pieces of data : a  node (job), categories (vector of  skills)
+;; place the 2 pieces of data in a map and return the map as our result.
 (defn parse-elem [elem]
    (let [ content (vec (:content elem))
      data-vals  (map (fn [z] (:content z)) content)
@@ -44,13 +49,11 @@
      categories (extract-categories data-vec)]
      (assoc {} :node (create-node title pubDate link job-key categories ) :categories categories)))
 
+;; filter the xml for items only
+;; appy our parse-elem function to each item (elem)
+;; package the results as a vector
 (defn dist-node-elems [data]
- (let [ data-vec  (vec
-  (for [ n data] (when (= :item (:tag n)) (parse-elem n) ) ))]
-   ;; remove any nils 
-   (filter #(when (not (nil? %1) ) %1) (distinct data-vec))
-))
-
+  (vec (map parse-elem (filter  #(if (= :item (:tag %1)) %1) data))))
 
 (defn get-xml-from-file [file_uri ]
   (let [ d (xml-seq  (xml/parse (java.io.File. file_uri))) ] d ))
