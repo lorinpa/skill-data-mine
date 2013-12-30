2becf3b3819a84ff2634a2801a88cfd177813146
Lorin Klugman <lorin@public-action.org>
2013-12-30 12:46:19 -0600

    Added new report logic. We can now report the percent of jobs a list skills occured per snapshot. E.G. whant percent of jobs contained java and python.

--- src/skill_data_mine/report.clj
+++ src/skill_data_mine/report.clj
@@ -1,30 +1,18 @@
-(ns skill-data-mine.report
-  (:require [clojure.xml :as xml])
-   (:use skill-data-mine.rss)
-   (:use skill-data-mine.persist)
-)
+(ns skill-data-mine.report)
 
-(use '[datomic.api :only [q db] :as d])
+(use 'skill-data-mine.persist)
 (use 'clojure.pprint)
 (use '(incanter core charts datasets svg))
 
 (defn display-job-stats [conn snapshot-description]
- (let [ res (get-job-skills-freq conn snapshot-description)
-        total (get-job-total conn snapshot-description)
-       ]
-    (doseq [n res]
-       (pprint (d/invoke (db conn) :calc-percent (nth n 0) total (int (nth n 1))  ) ))
- ))
+    (pprint (:rows (get-job-stats conn snapshot-description))))
 
 ;; text report allows to select the greatest values
 ;; E.G. top 10
 (defn display-top-stats [conn snapshot-description top]
- (let [ res (get-job-skills-freq conn snapshot-description)
-        total (get-job-total conn snapshot-description)
-        top-recs (take top (reverse (sort-by last res)))
-       ]
-    (doseq [n top-recs]
-       (pprint (d/invoke (db conn) :calc-percent (nth n 0) total (int (nth n 1))  ) ) )
+ (let [  rows  (:rows (get-job-stats conn snapshot-description))
+         top-recs (take top (reverse (sort-by last rows)))]
+       (pprint top-recs)
  ))
 
     
@@ -50,8 +38,6 @@
     (view (bar-chart (vec skills) (vec percents)  :title title :vertical false :x-label "Skill" :y-label "% of Job Postings"  ) )
  ))
 
-
-
 (defn display-skill-percent-history [conn skill]
   (let [jh (get-skill-history conn skill) ] 
     (view (bar-chart (vec (map first jh)) (vec (map last jh))  :title skill :vertical false :x-label "Skill" :y-label "% of Job Postings")) )
@@ -76,8 +62,8 @@
 
 (defn report-skill-history-filtered [conn skill filter-vec]
   (println (format "** Skill History: %s Filter : %s **" skill filter-vec))
-  
-  (pprint (get-skill-history-keyword-filter  conn skill (vec filter-vec)))
+  (pprint (get-skill-history-keyword-filter  conn skill (vec filter-vec))))
 
-  )
-
+(defn report-skill-list-history [conn skill-list]
+  (println (format "** Skill History:  %s **" skill-list))
+  (pprint (map (fn [a] [(:snapshot a) (:percent a)]) (report-skill-list-stats conn ["java" "javascript"]))))
